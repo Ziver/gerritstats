60774db4d75cdf4e7f58e4187667ea32068c7092
Ziver <ziver@koc.se>
2020-08-24 13:52:25 +0200

    Improved execution script with parameters

--- gerrit_stats.sh
+++ gerrit_stats.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
+
 #
 # Runs GerritStats, generating HTML output by default.
 #
 
-script_path="$(cd "$(dirname -- "$0")" || exit 1; pwd -P)"
-output_dir="$script_path/out-html"
+# ------------
+# Preparations
+# ------------
+
+script_path=$(dirname "$0")
+output_dir="${script_path}/out-html"
 
 new_args=()
-index=0
-next_is_output_dir=""
 
-for arg in "$@"; do
-    if [ "$next_is_output_dir" != "" ]; then
-        output_dir="$arg"
-        next_is_output_dir=""
-    elif [ "$arg" == "-o" ]; then
-        next_is_output_dir="1"
-    else
-        new_args[$index]="$arg"
-        let "index += 1"
-    fi
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --output-dir | -o)
+            output_dir=$2
+            shift 2
+            ;;
+
+        *)
+            new_args=(${new_args} $1)
+            shift 1
+            ;;
+    esac
 done
 
-cd "$script_path" || exit 1
-rm -rf GerritStats/out-html/data && \
-	java -Xmx4096m -Xms256m -jar GerritStats/build/libs/GerritStats.jar \
-    -o GerritStats/out-html/data "${new_args[@]}" || exit 1
+if [[ ! -d "${script_path}" ]]; then
+    echo "ERROR: Script path not available: ${script_path}"
+    exit 1
+fi
 
-cd "$script_path/GerritStats" || exit 1
-npm run webpack && \
-	mkdir -p "$output_dir/data" && \
-    cp -r "$script_path/GerritStats/out-html/data" "$output_dir/" && \
-    cp -r "$script_path/GerritStats/out-html/"* "$output_dir/"
+output_dir=$(realpath "${output_dir}")
+mkdir -p "${output_dir}/data"
 
-echo "Output generated to $output_dir"
+# -------------
+# Generate data
+# -------------
+
+java -Xmx4096m -Xms256m -jar "${script_path}/GerritStats/build/libs/GerritStats.jar" -o "${output_dir}/data" "${new_args[@]}"
+exit_code=$?
+
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "ERROR: Data processing failed, please check above log."
+    exit 1
+fi
+
+# ----------------
+# Generate website
+# ----------------
+
+cd "${script_path}/GerritStats" || exit
+npm run webpack -o "${output_dir}/bundle.js"
+exit_code=$?
+cd - || exit
+
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "ERROR: Webpack failed, please check above log."
+fi
+
+echo
+echo "Output generated to ${output_dir}"
